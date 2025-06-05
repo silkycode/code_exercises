44c59ba031006308df0f5e896010502b0a82cb37
silkycode <104239843+silkycode@users.noreply.github.com>
2025-06-05 03:03:36 -0700

    naive approach for q3 partial

--- 3.py
+++ 3.py
@@ -30,10 +30,33 @@ class Node:
     left: Optional['Node'] = None
     right: Optional['Node'] = None
 
+    def has_child(self):
+        if self.left or self.right != None:
+            return True
+
+def insert(root: Node, s: str):
+    if root.left == None:
+        root.left = Node(s)
+    elif root.right == None:
+        root.right = Node(s)
+    elif root.right != None:
+        left = root.left
+        insert(left, s)
+    elif root.left != None:
+        right = root.right
+        insert(right, s)
+
 def serialize(root: Node) -> str:
     result = ""
     return result
 
 def deserialize(s: str) -> Node:
-    return Node(val="b")
 
+    root = Node(s[0])
+    for char in s[1:]:
+        insert(root, char)
+    return root
+
+
+tree = deserialize("a super long sentence")
+print(tree)
